a63b6b616c3869beaec730eed62e8e276d6434d2
Aanas Sayed <aanas.sayed@outlook.com>
2024-07-30 20:54:49 +0100

    chore: linting with pylint

--- tests/array_block_visualizer/test_errors.py
+++ tests/array_block_visualizer/test_errors.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest  # pylint: disable=unused-import
 from arrayviz import ArrayBlockVisualizer
 from tests.utils import dummy_func
 
@@ -12,6 +12,12 @@ from tests.utils import dummy_func
     ],
 )
 def test_array_count(arrays, pointers, expected_array_count):
+    """
+    Test the count of arrays recorded by ArrayBlockVisualizer.
+
+    Verifies that the array count recorded by the visualizer matches the expected count
+    based on the provided arrays and pointers.
+    """
     visualizer = ArrayBlockVisualizer(func=dummy_func, func_args=([],), delay=0.5)
 
     visualizer.record_frame(arrays=arrays, pointers=pointers)
@@ -28,21 +34,33 @@ def test_array_count(arrays, pointers, expected_array_count):
     ],
 )
 def test_array_count_mismatch(arrays_1, pointers_1, arrays_2, pointers_2):
+    """
+    Test mismatched array counts in record_frame method.
+
+    Ensures that a ValueError is raised when attempting to record frames with mismatched
+    array counts or pointer counts.
+    """
     visualizer = ArrayBlockVisualizer(func=dummy_func, func_args=([],), delay=0.5)
 
     visualizer.record_frame(arrays=arrays_1, pointers=pointers_1)
 
-    with pytest.raises(ValueError) as e:
+    with pytest.raises(ValueError):
         visualizer.record_frame(arrays=arrays_2, pointers=pointers_2)
 
 
 def test_record_frame_missing_arguments():
+    """
+    Test the behavior of record_frame with missing arguments.
+
+    Verifies that AttributeError is raised when record_frame is called with missing
+    arrays and pointers arguments.
+    """
     visualizer = ArrayBlockVisualizer(func=dummy_func, func_args=([],), delay=0.5)
 
-    with pytest.raises(AttributeError) as e:
+    with pytest.raises(AttributeError):
         visualizer.record_frame(arrays=None, pointers=None)
 
     visualizer.record_frame(arrays=[[1, 2, 3]], pointers=[{"i": 0}])
 
-    with pytest.raises(AttributeError) as e:
+    with pytest.raises(AttributeError):
         visualizer.record_frame(arrays=None, pointers=None)
--- tests/array_block_visualizer/test_function.py
+++ tests/array_block_visualizer/test_function.py
@@ -1,10 +1,18 @@
-import pytest
+import pytest  # pylint: disable=unused-import
 from arrayviz import ArrayBlockVisualizer
 from tests.utils.functions import dummy_func
 
 
-# NOTE: Modifying the same array is modifed and passed inside dummy_func to test deep copy
 def test_func_execution():
+    """
+    Test the execution of a function with ArrayBlockVisualizer.
+
+    Verifies that the visualizer correctly constructs frames based on the function execution,
+    and checks that the frames list contains the expected states of arrays and pointers.
+
+    Also tests if deep copy is implemented as `dummy_func` modifies arrays by referrence before
+    pushing frame.
+    """
     visualizer = ArrayBlockVisualizer(func=dummy_func, func_args=(), delay=0.5)
 
     visualizer.construct()
